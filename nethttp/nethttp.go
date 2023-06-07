735f61cc594b8fbcac31bc71dedfbcd5d04c6421
Daniel Antonsen <44880186+root4loot@users.noreply.github.com>
2023-06-07 14:17:04 +0200

    improve resolver function
    - try default resolvers first
    - use IP cache to store and reuse resolved IP addresses

--- nethttp/nethttp.go
+++ nethttp/nethttp.go
@@ -6,59 +6,28 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"sync"
 )
 
-// ClientWithCustomResolvers creates an HTTP client with custom resolvers list
-func ClientWithCustomResolvers(resolvers []string) (*http.Client, error) {
-	// Check if the resolvers contain the port :53, if not, add it
-	for i, resolver := range resolvers {
-		_, _, err := net.SplitHostPort(resolver)
-		if err != nil {
-			// Port is not specified, add :53
-			resolvers[i] = resolver + ":53"
+var ipCache sync.Map
+
+// ClientWithOptionalResolvers creates an HTTP client with default and custom resolvers that are
+// optionally provided. If no resolvers are provided, the default resolver is used,
+// utilizing an IP cache to store and reuse resolved IP addresses.
+func ClientWithOptionalResolvers(resolvers ...string) (*http.Client, error) {
+	// Check if resolvers are provided
+	if len(resolvers) > 0 {
+		// Check if the resolvers contain the port :53, if not, add it
+		for i, resolver := range resolvers {
+			_, _, err := net.SplitHostPort(resolver)
+			if err != nil {
+				// Port is not specified, add :53
+				resolvers[i] = resolver + ":53"
+			}
 		}
-	}
-
-	dialer := &net.Dialer{
-		Resolver: &net.Resolver{
-			PreferGo: true,
-			Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
-				for _, resolver := range resolvers {
-					d := net.Dialer{}
-					conn, err := d.DialContext(ctx, "udp", resolver)
-					if err != nil {
-						continue // Try the next resolver
-					}
-					return conn, nil // Success, return the connection
-				}
-				return nil, fmt.Errorf("failed to resolve domain using custom resolvers")
-			},
-		},
-	}
-
-	transport := &http.Transport{
-		DialContext:         dialer.DialContext,
-		TLSClientConfig:     &tls.Config{InsecureSkipVerify: true},
-		DisableKeepAlives:   true,
-		MaxIdleConnsPerHost: -1,
-	}
-
-	client := &http.Client{
-		Transport: transport,
-	}
-
-	return client, nil
-}
-
-// ClientWithDefaultAndCustomResolvers creates an HTTP client with default and custom resolvers
-func ClientWithDefaultAndCustomResolvers(resolvers []string) (*http.Client, error) {
-	// Check if the resolvers contain the port :53, if not, add it
-	for i, resolver := range resolvers {
-		_, _, err := net.SplitHostPort(resolver)
-		if err != nil {
-			// Port is not specified, add :53
-			resolvers[i] = resolver + ":53"
-		}
+	} else {
+		// No resolvers provided, use an empty slice
+		resolvers = []string{}
 	}
 
 	defaultResolver := &net.Resolver{PreferGo: true}
@@ -69,6 +38,14 @@ func ClientWithDefaultAndCustomResolvers(resolvers []string) (*http.Client, erro
 
 	transport := &http.Transport{
 		DialContext: func(ctx context.Context, network, address string) (net.Conn, error) {
+			// Check if the resolved IP address exists in the cache
+			if cachedIP, ok := ipCache.Load(address); ok {
+				if conn, ok := cachedIP.(net.Conn); ok {
+					return conn, nil // Reuse the connection
+				}
+			}
+
+			// IP address not found in cache, perform DNS lookup
 			conn, err := dialer.DialContext(ctx, network, address)
 			if err != nil {
 				for _, resolver := range resolvers {
@@ -77,10 +54,18 @@ func ClientWithDefaultAndCustomResolvers(resolvers []string) (*http.Client, erro
 					if err != nil {
 						continue // Try the next resolver
 					}
+
+					// Store the resolved IP address in the cache for future reuse
+					ipCache.Store(address, conn)
+
 					return conn, nil // Success, return the connection
 				}
 				return nil, fmt.Errorf("failed to resolve domain using custom resolvers")
 			}
+
+			// Store the resolved IP address in the cache for future reuse
+			ipCache.Store(address, conn)
+
 			return conn, nil // Success, return the connection
 		},
 		TLSClientConfig:     &tls.Config{InsecureSkipVerify: true},
