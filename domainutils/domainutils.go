84145a61319804e427c77b8caef354ead3987bbb
Daniel Antonsen <44880186+root4loot@users.noreply.github.com>
2023-07-04 16:02:54 +0200

    Revert "update package names"
    This reverts commit a454334cf3bc2338acc0d3be2f9719c92791475e.

--- domainutils/domainutils.go
+++
@@ -1,9 +0,0 @@
-package netdomain
-
-import "regexp"
-
-func IsWildcardHostname(hostname string) bool {
-	hostnamePattern := `^(\*\.)?[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)*$`
-	match, _ := regexp.MatchString(hostnamePattern, hostname)
-	return match
-}
