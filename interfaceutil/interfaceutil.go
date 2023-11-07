177af9f9f5bb718470ec9a90ef8b83f3195481fa
Daniel Antonsen <44880186+root4loot@users.noreply.github.com>
2023-11-07 11:50:38 +0100

    add func Contains

--- interfaceutil/interfaceutil.go
+++ interfaceutil/interfaceutil.go
@@ -1,1 +1,20 @@
 package interfaceutil
+
+import "reflect"
+
+// Contains checks if an item is present in a list.
+func Contains(list interface{}, item interface{}) bool {
+	listValue := reflect.ValueOf(list)
+
+	if listValue.Kind() != reflect.Slice {
+		panic("List argument must be a slice")
+	}
+
+	for i := 0; i < listValue.Len(); i++ {
+		if reflect.DeepEqual(listValue.Index(i).Interface(), item) {
+			return true
+		}
+	}
+
+	return false
+}
