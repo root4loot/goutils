fea7cb49ea5465ae83f25246c60211a837b3f9a1
Daniel Antonsen <44880186+root4loot@users.noreply.github.com>
2023-11-07 11:15:19 +0100

    add fuc IsDomainName

--- domainutil/domainutil.go
+++ domainutil/domainutil.go
@@ -1,6 +1,9 @@
 package domainutil
 
-import "regexp"
+import (
+	"net/url"
+	"regexp"
+)
 
 // IsDomainName checks if a string is a valid domain name.
 func IsDomainName(str string) bool {
@@ -8,4 +11,15 @@ func IsDomainName(str string) bool {
 	regex := regexp.MustCompile(`^(?i)(\*\.){0,1}(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.){1,}(?:[a-z]{2,})$`)
 
 	return regex.MatchString(str)
+}
+
+// IsValidDomain checks if a domain is valid by attempting to parse it with a custom scheme.
+func IsValidDomain(domain string) bool {
+	u, err := url.Parse("valid://" + domain)
+	if err != nil {
+		return false
+	}
+
+	// Check if the host is non-empty and there's no Path or RawQuery
+	return u.Host != "" && u.Path == "" && u.RawQuery == ""
 }
