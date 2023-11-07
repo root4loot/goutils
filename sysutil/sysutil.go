db3dac0e1c275c5c698173bfd15db0e8e64446ea
Daniel Antonsen <44880186+root4loot@users.noreply.github.com>
2023-11-07 11:40:02 +0100

    add IsRoot

--- sysutil/sysutil.go
+++ sysutil/sysutil.go
@@ -1,1 +1,15 @@
 package sysutil
+
+import (
+	"log"
+	"os/user"
+)
+
+// IsRoot checks if the current user is root.
+func IsRoot() bool {
+	currentUser, err := user.Current()
+	if err != nil {
+		log.Fatalf("[isRoot] Unable to get current user: %s", err)
+	}
+	return currentUser.Username == "root"
+}
