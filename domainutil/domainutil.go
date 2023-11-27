cdf1f83e2078e957f97c5952c4917da2136cb027
Daniel Antonsen <44880186+root4loot@users.noreply.github.com>
2023-11-27 14:04:30 +0100

    add func IsURL

--- domainutil/domainutil.go
+++ domainutil/domainutil.go
@@ -6,9 +6,14 @@ import (
 	"strings"
 )
 
+// IsURL checks if a string is a URL.
+func IsURL(url string) bool {
+	regex := regexp.MustCompile(`^(https?|ftp)://[^\s/$.?#].[^\s]*$`)
+	return regex.MatchString(url)
+}
+
 // IsDomainName checks if a string is a valid domain name.
 func IsDomainName(str string) bool {
-	// Regular expression pattern to match domain names with optional wildcards
 	regex := regexp.MustCompile(`^(?i)(\*\.){0,1}(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.){1,}(?:[a-z]{2,})$`)
 
 	return regex.MatchString(str)
