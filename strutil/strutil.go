29f3e02f0fd15593dbe50e3419126701a4226a4f
Daniel Antonsen <44880186+root4loot@users.noreply.github.com>
2023-12-08 18:36:59 +0100

    Add function to trim whitespace from string

--- strutil/strutil.go
+++ strutil/strutil.go
@@ -1,1 +1,9 @@
 package strutil
+
+import "strings"
+
+// TrimWhitespace removes all whitespace from a string.
+func TrimWhitespace(str string) string {
+	fields := strings.Fields(str)
+	return strings.Join(fields, "")
+}
