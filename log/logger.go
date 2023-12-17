31ec1a635ba1c15258edd873165087bf7219a4c8
Daniel Antonsen <44880186+root4loot@users.noreply.github.com>
2023-12-17 10:25:00 +0100

    Add comments

--- log/logger.go
+++ log/logger.go
@@ -46,6 +46,7 @@ func Init(name string) {
 	log = NewLogger(name)
 }
 
+// String returns the string representation of the log level
 func (lvl Level) String() string {
 	return levels[lvl]
 }
@@ -57,10 +58,12 @@ func NewLogger(packageName string) *Logger {
 	return &Logger{l, packageName}
 }
 
+// NewTag creates a new tag
 func NewTag(tag string) *Tag {
 	return &Tag{tag: tag}
 }
 
+// NewTag creates a new tag
 func (l *Logger) NewTag(tag string) *Tag {
 	return &Tag{logger: l, tag: tag}
 }
@@ -76,10 +79,12 @@ func SetLevelStr(level string) {
 	log.Logger.SetLevel(convertRelogLevelToLogrusLevel(getLogLevel(level)))
 }
 
+// GetLevel returns the logger level
 func GetLevel() Level {
 	return convertLogrusLevelToRelogLevel(log.Logger.GetLevel())
 }
 
+// WithFields returns a logrus entry with the fields set
 func WithFields(fields Fields) *logrus.Entry {
 	return log.Logger.WithFields(logrus.Fields(fields))
 }
@@ -93,6 +98,7 @@ func (t *Tag) entry() *logrus.Entry {
 	}
 }
 
+// Log logs a message with the tag
 func (t *Tag) Log(v ...interface{}) {
 	entry := t.entry()
 	if entry != nil {
@@ -100,6 +106,7 @@ func (t *Tag) Log(v ...interface{}) {
 	}
 }
 
+// Logf logs a formatted message with the tag
 func (t *Tag) Logf(format string, v ...interface{}) {
 	entry := t.entry()
 	if entry != nil {
@@ -107,61 +114,70 @@ func (t *Tag) Logf(format string, v ...interface{}) {
 	}
 }
 
-// Generalized logging functions that accept any type as an argument
+// Debug logs a message at level Debug on the standard logger.
 func Debug(v ...interface{}) {
 	if log != nil {
 		log.Debug(strings.TrimSpace(fmt.Sprintln(v...)))
 	}
 }
+
+// Info logs a message at level Info on the standard logger.
 func Info(v ...interface{}) {
 	if log != nil {
 		log.Info(strings.TrimSpace(fmt.Sprintln(v...)))
 	}
 }
 
+// Warn logs a message at level Warn on the standard logger.
 func Warn(v ...interface{}) {
 	if log != nil {
 		log.Warn(strings.TrimSpace(fmt.Sprintln(v...)))
 	}
 }
 
+// Error logs a message at level Error on the standard logger.
 func Error(v ...interface{}) {
 	if log != nil {
 		log.Error(strings.TrimSpace(fmt.Sprintln(v...)))
 	}
 }
 
+// Fatal logs a message at level Fatal on the standard logger.
 func Fatal(v interface{}) {
 	if log != nil {
 		log.Fatal(strings.TrimSpace(fmt.Sprintln(v)))
 	}
 }
 
-// Formatted logging functions
+// Debugf logs a message at level Debug on the standard logger.
 func Debugf(format string, v ...interface{}) {
 	if log != nil {
 		log.Debugf(format, v...)
 	}
 }
 
+// Infof logs a message at level Info on the standard logger.
 func Infof(format string, v ...interface{}) {
 	if log != nil {
 		log.Infof(format, v...)
 	}
 }
 
+// Warnf logs a message at level Warn on the standard logger.
 func Warnf(format string, v ...interface{}) {
 	if log != nil {
 		log.Warnf(format, v...)
 	}
 }
 
+// Errorf logs a message at level Error on the standard logger.
 func Errorf(format string, v ...interface{}) {
 	if log != nil {
 		log.Errorf(format, v...)
 	}
 }
 
+// Fatalf logs a message at level Fatal on the standard logger.
 func Fatalf(format string, v ...interface{}) {
 	if log != nil {
 		log.Fatalf(format, v...)
