8d2184b6475c8a857c9c076b86f85f2b5a76f0e4
Daniel Antonsen <44880186+root4loot@users.noreply.github.com>
2024-06-17 16:52:29 +0200

    Add func to determine if content type is text-based

--- sysutil/sysutil.go
+++ sysutil/sysutil.go
@@ -2,6 +2,7 @@ package sysutil
 
 import (
 	"os/user"
+	"strings"
 )
 
 // IsRoot checks if the current user is root.
@@ -11,4 +12,31 @@ func IsRoot() (bool, error) {
 		return false, err
 	}
 	return currentUser.Username == "root", nil
+}
+
+// IsTextContentType checks if a content type is text-based
+func IsTextContentType(contentType string) bool {
+	var nonTextContentTypes = []string{
+		"application/octet-stream",
+		"application/pdf",
+		"application/zip",
+		"application/x-gzip",
+		"application/vnd.ms-excel",
+		"application/vnd.ms-powerpoint",
+		"application/vnd.ms-word",
+	}
+
+	for _, nonTextType := range nonTextContentTypes {
+		if nonTextType == contentType {
+			return false
+		}
+	}
+
+	// Check for generic types
+	part := strings.Split(contentType, "/")[0]
+	if part == "image" || part == "audio" || part == "video" {
+		return false
+	}
+
+	return true
 }
