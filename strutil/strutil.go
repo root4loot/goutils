2efe629c50401d5c6b45e11aec6eb11d9e789c57
Daniel Antonsen <44880186+root4loot@users.noreply.github.com>
2024-06-17 15:50:43 +0200

    Add func to check if string is printable

--- strutil/strutil.go
+++ strutil/strutil.go
@@ -1,9 +1,23 @@
 package strutil
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+	"unicode"
+)
 
 // TrimWhitespace removes all whitespace from a string.
 func TrimWhitespace(str string) string {
 	fields := strings.Fields(str)
 	return strings.Join(fields, "")
+}
+
+// IsPrintable checks if a string is printable
+func IsPrintable(str string) bool {
+	for _, r := range str {
+		if !unicode.IsPrint(r) && !strconv.IsPrint(r) {
+			return false
+		}
+	}
+	return true
 }
